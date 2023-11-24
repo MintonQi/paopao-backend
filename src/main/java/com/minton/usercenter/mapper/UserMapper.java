4992a840be63a0a2fddd42b81e3b7f2f95a411ea
mingtongqi <mingtongqi@creditease.cn>
2023-11-24 17:44:46 +0800

    init

---
+++ src/main/java/com/minton/usercenter/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.minton.usercenter.mapper;
+
+import com.minton.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
---
+++ src/main/java/com/minton/usercenter/model/domain/request/UserRegisterRequest.java
@@ -0,0 +1,31 @@
+package com.minton.usercenter.model.domain.request;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class UserRegisterRequest implements Serializable {
+
+    private static final long serialVersionUID = 3191241716373120793L;
+
+    /**
+     * 用户账号
+     */
+    private String userAccount;
+
+    /**
+     * 用户密码
+     */
+    private String userPassword;
+
+    /**
+     * 校验密码
+     */
+    private String checkPassword;
+
+    /**
+     * 星球编号
+     */
+    private String planetCode;
+}
